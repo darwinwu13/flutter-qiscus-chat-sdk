9bd4760d78da491cf372bcff64ecf98e5146015a
Hansen <hansenwu98@gmail.com>
2020-06-08 17:40:14 +0700

    first commit

--- ios/Classes/SwiftQiscusSdkPlugin.swift
+++ ios/Classes/SwiftQiscusSdkPlugin.swift
@@ -10,5 +10,31 @@ public class SwiftQiscusSdkPlugin: NSObject, FlutterPlugin {
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     result("iOS " + UIDevice.current.systemVersion)
+    
+    switch call.method {
+    case "getQiscusAccount":
+        getQiscusAccount(withResult: result)
+        break
+    default:
+        return
+    }
   }
+//    private void getQiscusAccount(Result result) {
+//        try {
+//            result.success(QiscusSdkHelper.encodeQiscusAccount(QiscusCore.getQiscusAccount()));
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            result.error("ERR_FAILED_GET_ACCOUNT", e.getMessage(), e);
+//        }
+//
+//    }
+
+    private func getQiscusAccount(withResult result: FlutterResult){
+        do {
+            try result(QiscusSdkPlugin)
+        } catch (e: Error) {
+            
+        }
+    }
 }
