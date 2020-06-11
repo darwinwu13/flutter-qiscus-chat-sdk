41e5b38fce3fd8a03a7deb2ddfb46ac8d7394268
Hansen <hansenwu98@gmail.com>
2020-06-11 15:03:00 +0700

    add any methods in ios plugin

---
+++ ios/Classes/GenerateToDictionary.swift
@@ -0,0 +1,14 @@
+//
+//  GenerateToDictonary.swift
+//  Alamofire
+//
+//  Created by bahaso on 11/06/20.
+//
+
+import Foundation
+
+class GenerateToDictionary {
+    public func userModel() -> [String: Any] {
+        return [:]
+    }
+}
